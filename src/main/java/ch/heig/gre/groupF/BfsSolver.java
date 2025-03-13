72af3bd03ec538d416fb9738db29e6897c37a34f
skyrag <duprat.nicolas@outlook.com>
2025-03-13 15:23:10 +0100

    rendu

--- src/main/java/ch/heig/gre/groupF/BfsSolver.java
+++ src/main/java/ch/heig/gre/groupF/BfsSolver.java
@@ -3,12 +3,46 @@ package ch.heig.gre.groupF;
 import ch.heig.gre.graph.Graph;
 import ch.heig.gre.graph.VertexLabelling;
 import ch.heig.gre.maze.MazeSolver;
+import ch.heig.gre.maze.Progression;
 
 import java.util.*;
 
 public final class BfsSolver implements MazeSolver {
   @Override
   public List<Integer> solve(Graph graph, int source, int destination, VertexLabelling<Integer> treatments) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    Queue<Integer> queue = new LinkedList<>();
+    Map<Integer, Integer> parent = new HashMap<>();
+    treatments.setLabel(source, 0);
+    parent.put(source, null);
+    queue.add(source);
+
+    while (!queue.isEmpty()) {
+
+      int current = queue.poll();
+      if (current == destination) {
+        return getPath(destination, parent);
+      }
+
+      for ( int i : graph.neighbors(current)) {
+        if (!parent.containsKey(i)) {
+          queue.add(i);
+          parent.put(i, current);
+          treatments.setLabel(i, treatments.getLabel(current) + 1);
+        }
+      }
+    }
+    return Collections.emptyList();
+  }
+
+
+  private List<Integer> getPath(int destination, Map<Integer, Integer> parent) {
+    List<Integer> path = new ArrayList<>();
+    for (Integer at = destination; at != null; at = parent.get(at)) {
+      path.add(at);
+    }
+    Collections.reverse(path);
+    return path;
   }
 }
+
+
--- src/main/java/ch/heig/gre/groupF/DfsGenerator.java
+++ src/main/java/ch/heig/gre/groupF/DfsGenerator.java
@@ -2,19 +2,39 @@ package ch.heig.gre.groupF;
 
 import ch.heig.gre.maze.MazeBuilder;
 import ch.heig.gre.maze.MazeGenerator;
+import ch.heig.gre.maze.Progression;
 
-import java.util.Stack;
+import java.util.*;
 
-// TODO: renommer le package (Shift + F6) selon la lettre attribuée à votre groupe
 
 public final class DfsGenerator implements MazeGenerator {
   @Override
   public void generate(MazeBuilder builder, int from) {
+    Stack<Integer> stack = new Stack<>();
+    builder.progressions().setLabel(from, Progression.PROCESSED);
+    stack.push(from);
+    Random rand = new Random();
 
+    while (!stack.isEmpty()) {
+      int v = stack.pop();
+      List<Integer> neighbours = builder.topology().neighbors(v);
+
+      Collections.shuffle(neighbours, rand);
+
+      neighbours.removeIf(neighbour -> !builder.progressions().getLabel(neighbour).equals(Progression.PENDING));
+
+      if (!neighbours.isEmpty()) {
+        int neighbour = neighbours.get(rand.nextInt(neighbours.size()));
+        stack.push(v);
+        builder.removeWall(neighbour, v);
+        builder.progressions().setLabel(neighbour, Progression.PROCESSED);
+        stack.push(neighbour);
+      }
+    }
   }
 
   @Override
   public boolean requireWalls() {
-    throw new UnsupportedOperationException("Not implemented yet");
+    return true;
   }
 }
